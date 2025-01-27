df3e610a7b59c541e01eaf50d3e2c0da419c9e7e
saqib-suleman <saqibs3291@gmail.com>
2025-01-28 02:11:43 +0500

    queue and stack

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,39 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+};
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
+        const node = { value: item } as Node<T>;
+        this.length++;
 
-}
+        if (!this.tail) {
+            this.head = this.tail = node;
+            return;
+        }
+
+        this.tail.next = node;
+        this.tail = node;
+    }
     deque(): T | undefined {
-
-}
+        if (!this.head) return undefined;
+        const currHead = this.head;
+        this.head = currHead?.next;
+        currHead.next = undefined;
+        this.length--;
+        return currHead?.value;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,47 @@
+type Node<T> = {
+    value: T;
+    prev?: Node<T>;
+};
+
 export default class Stack<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
 
     constructor() {
+        this.head = undefined;
+        this.length = 0;
     }
 
     push(item: T): void {
+        this.length++;
 
-}
+        const node = { value: item } as Node<T>;
+
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+
+        node.prev = this.head;
+        this.head = node;
+    }
+
     pop(): T | undefined {
+        this.length--;
 
-}
+        if (!this.head) {
+            const currHead = this.head;
+            this.head = undefined;
+            // @ts-ignore
+            return currHead?.value;
+        }
+
+        const currHead = this.head;
+        this.head = this.head?.prev;
+        return currHead.value;
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
