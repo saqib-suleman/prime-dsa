ae8327513adbfdb0b733a016398e4db67c32abf9
saqib-suleman <saqibs3291@gmail.com>
2025-01-27 22:41:39 +0500

    two crystal balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,18 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    let index = -1;
+    let left = 0;
+    let sqrt = Math.floor(Math.sqrt(breaks.length - 1));
 
+    while (left < breaks.length && !breaks[left + sqrt]) {
+        left += sqrt;
+    }
+
+    for (let i = left + 1; i < breaks.length - 1; i++) {
+        if (breaks[i]) {
+            index = i;
+            break;
+        }
+    }
+
+    return index;
 }
