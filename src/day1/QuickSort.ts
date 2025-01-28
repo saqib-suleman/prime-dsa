269d02d32af558c3d63d54b7d51757f33b5b5ae2
saqib-suleman <saqibs3291@gmail.com>
2025-01-29 03:27:01 +0500

    quick sort

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,36 @@
-export default function quick_sort(arr: number[]): void {
+function partition(arr: number[], lo: number, hi: number) {
+    let pivot = arr[lo];
+    let start = lo;
+    let end = hi;
 
+    while (start < end) {
+        while (arr[start] <= pivot) {
+            start++;
+        }
+        while (arr[end] > pivot) {
+            end--;
+        }
+        if (start < end) {
+            let temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+        }
+    }
+    let temp = arr[end];
+    arr[end] = arr[lo];
+    arr[lo] = temp;
+
+    return end;
+}
+
+function sort(arr: number[], lo: number, hi: number) {
+    if (lo < hi) {
+        const sortedIndex = partition(arr, lo, hi);
+        sort(arr, lo, sortedIndex - 1);
+        sort(arr, sortedIndex + 1, hi);
+    }
+}
+
+export default function quick_sort(arr: number[]): void {
+    sort(arr, 0, arr.length - 1);
 }
