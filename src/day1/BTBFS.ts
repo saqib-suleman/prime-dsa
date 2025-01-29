4eb431397491a1f4d572a7dbe4492b3badb30884
saqib-suleman <saqibs3291@gmail.com>
2025-01-29 18:24:47 +0500

    tree traversal

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,20 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const q = [head];
+    const path = [];
 
+    while (q.length) {
+        const curr = q.shift();
+        path.push(curr?.value);
+
+        if (curr?.value === needle) {
+            return true;
+        }
+
+        if (curr?.left) q.push(curr?.left);
+        if (curr?.right) q.push(curr?.right);
+    }
+
+    console.log(path);
+
+    return false;
 }
--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,17 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {
+function walk(node: BinaryNode<number> | null, path: number[]) {
+    if (!node) {
+        return;
+    }
 
+    path.push(node.value);
+    walk(node.left, path);
+    walk(node.right, path);
+
+    return;
+}
+
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+    const path: number[] = [];
+    walk(head, path);
+    return path;
 }
